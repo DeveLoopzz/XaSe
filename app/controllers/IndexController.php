23ac2ab960f6044927f3698f93fa4a21713bbfe9
DeveLoopzz <develoopzz00@gmail.com>
2024-10-09 12:34:43 +0200

    index controller creation

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+Class IndexController extends Controller {
+
+    public function indexAction() {
+        echo ROOT_PATH;
+    }
+
+    public function updateAction() {
+        
+    }
+
+    public function deleteAction() {
+       
+    }
+
+}
