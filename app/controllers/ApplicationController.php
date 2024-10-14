69bd6de3699d1151a0a40b5af3e0d790d6696eea
Xavi <149520332+llotjer@users.noreply.github.com>
2024-10-14 17:32:18 +0200

    Update ApplicationController.php

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -6,16 +6,4 @@
  */
 class ApplicationController extends Controller {
 	
-/*     public function addAction()
-    {
-        // 1. Agafar les dades del formulari
-        $taskText = $_POST['task'];
-        
-        // 2. Cridar al Model per guardar la tasca
-        $taskModel = new TaskModel();
-        $taskModel->addTask($taskText);
-        
-        // 3. Redirigir a la vista que mostra la llista de tasques
-        header("Location: /tasks");
-    } */
 }
