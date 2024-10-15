cfd2043f4c7fc16bff4ef5326f0ac5225fe72c90
DeveLoopzz <develoopzz00@gmail.com>
2024-10-15 10:14:22 +0200

    Update and delete function it should be separated but sometimes things dont go as they should be, alexa play sad songs

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -2,11 +2,54 @@
 
 class  TaskModel {
 
-    protected $filepath;
+    private $filepath = __DIR__ . '/../data/data.json';
 
-    function __construct(){
-        $this->filepath = __DIR__ . '/../data/data.json';
+    public function loadTasks() {
+        $data = file_get_contents($this->filepath);
+        return json_decode($data, true); 
+    }
+
+    public function searchTask($id) {
+        $datajson = $this->loadTasks();
+        $tasks = $datajson['tareas'];
+        foreach($tasks as $task) {
+            if($task['id'] == $id) {
+                return $task;
+            }
+        }
+        return null;
     }
 
     
+    public function updateTask($id, $nombre, $estado, $fechaInicio, $fechaFin, $user) {
+        $datajson = $this->loadTasks();
+        $tasks = $datajson['tareas'];
+
+        foreach ($tasks as &$task) {
+            if ($task['id'] == $id) {
+                $task['nombre'] = $nombre;
+                $task['estado'] = $estado;
+                $task['fechaInicio'] = $fechaInicio;
+                $task['fechaFin'] = $fechaFin;
+                $task['user'] = $user;
+                break;
+            }
+        }
+        file_put_contents($this->filepath, json_encode($tasks, JSON_PRETTY_PRINT));
+        }
+
+    public function deleteTask($id) {
+        $datajson = $this->loadTasks();
+        $tasks = $datajson['tareas'];
+        foreach($tasks as $index => $value) {
+            if($value['id'] == $id) {
+                unset($tasks[$index]);
+                 break;
+            }
+        }
+        $tasks = array_values($tasks);
+        $datajson['tareas'] = $tasks;
+        file_put_contents($this->filepath, json_encode($datajson, JSON_PRETTY_PRINT));
+    }
+
 }
