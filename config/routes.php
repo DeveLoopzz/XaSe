5626e397ab749753348132f11c0e0a98d9f03206
Xavi <149520332+llotjer@users.noreply.github.com>
2024-10-21 11:32:33 +0200

    Final changes in create & read views. Added lang mode.

--- config/routes.php
+++ config/routes.php
@@ -12,8 +12,17 @@
 $routes = array(
 	'/test' => 'test#index',
 	'/' => 'task#index',
+	'/lang' => 'task#lang',
 	'/create' => 'task#create',
 	'/read' => 'task#read',
+	'/update' => 'task#update',
+	'/delete' => 'task#delete',
 	'/add' => 'task#add',
+	'/confirmUpdate' => 'task#confirmUpdate',
+	'/updateTask' => 'task#taskUpdate',
+	'/deleteTask' => 'task#taskDelete',
+	'/confirmDelete' => 'task#confirmDelete',
 	'/success' => 'task#success',
+	'/successUpdate' => 'task#successUpdate',
+	'/successDelete' => 'task#successDelete',
 );
