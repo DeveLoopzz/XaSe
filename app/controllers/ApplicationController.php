72389fc2f2b92c2ab2d9a7af9813f0d6b8ad334b
DeveLoopzz <develoopzz00@gmail.com>
2024-10-23 11:49:13 +0200

    app files

---
+++ app/controllers/ApplicationController.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Base controller for the application.
+ * Add general things in this controller.
+ */
+class ApplicationController extends Controller 
+{
+	public function index(){
+
+    }
+    
+    
+}
