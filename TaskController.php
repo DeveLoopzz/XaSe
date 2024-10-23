dff34008804dc23a6ab9f53231214c3b38c4af7d
DeveLoopzz <develoopzz00@gmail.com>
2024-10-23 11:22:45 +0200

    Task Controller UD
    Task controller update and delete controllers

---
+++ TaskController.php
@@ -0,0 +1,32 @@
+<?php
+
+class TaskController {
+
+    private $model;
+
+    public function __construct($model) {
+        $this->model = $model;
+    }
+
+    public function updateTask($id, $nombre, $estado, $fechaInicio, $fechaFin, $user){
+        $task = $this->model->searchTask($id);
+        if($task) {
+            $this->model->updateTask($id, $nombre, $estado, $fechaInicio, $fechaFin, $user);
+            exit();
+        } else {
+            echo "task not found";
+        }
+    }
+
+    public function delteTask($id) {
+        $task = $this->model->searchTask($id);
+        if($task) {
+            $this->model->deleteTask($id);
+            exit();
+        } else {
+            echo "task not found";
+        }
+    }
+
+
+}
