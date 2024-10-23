8c6928132a77acb55128ce036268ce36a8cefcbc
Xavi <149520332+llotjer@users.noreply.github.com>
2024-10-23 11:32:52 +0200

    Update TaskController.php
    Added index, create, read, succes and lang controllers & add function

--- TaskController.php
+++ TaskController.php
@@ -8,6 +8,48 @@ class TaskController extends Controller{
         $this->model = new TaskModel;
     }
 
+    public function indexAction(){
+        
+        $this->view->lang = $_SESSION['lang'] ?? 'eng';
+    
+    }
+
+    public function createAction(){
+
+        $this->view->lang = $_SESSION['lang'] ?? 'eng';
+
+    }
+    
+    public function addAction(){
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $user = $_POST['user'] ?? '';
+            $title = $_POST['title'] ?? '';
+            $status = $_POST['status'] ?? '';
+            $start_date = $_POST['start_date'] ?? '';
+            $end_date = $_POST['end_date'] ?? '';
+        }
+
+        $result = $this->model->addTask($user, $title, $status, $start_date, $end_date);
+
+        if(!$result){
+            echo 'Error saving task.';
+            exit;
+        } else {
+            header("Location:" . WEB_ROOT . "/success");
+        }
+
+    }
+
+    public function readAction(){
+
+        $this->view->lang = $_SESSION['lang'] ?? 'eng';
+        
+        $tasks = $this->model->readTasks();
+        
+        $this->view->tasks = $tasks;
+    }
+
     public function updateAction(){
         $this->view->lang = $_SESSION['lang'] ?? 'eng';
         $tasks = $this->model->readTasks();
@@ -76,12 +118,30 @@ class TaskController extends Controller{
         }
     }
 
+    public function successAction(){
+
+        $this->view->lang = $_SESSION['lang'] ?? 'eng';
+
+    }
+
     public function successUpdateAction(){
         $this->view->lang = $_SESSION['lang'] ?? 'eng';
     }
 
     public function successDeleteAction(){
         $this->view->lang = $_SESSION['lang'] ?? 'eng';
+    }
+
+    public function langAction(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $lang = $_POST['lang'] ?? '';
+            $this->view->lang = $lang;
+            $_SESSION['lang'] = $lang;
+        }
+
+        $this->view->lang = $_SESSION['lang'] ?? 'eng';
+        header("Location: " . WEB_ROOT);
+        exit();
     }
 
 }
